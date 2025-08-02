511ed609da6ac3cd80d1652de37c6d75d4b9923a
satyajit9178 <satyajits104@gmail.com>
2025-08-02 23:15:15 +0530

    Add a data folder

---
+++ data/projects.js
@@ -0,0 +1,36 @@
+export const projects = [{
+  name:"Taskflow-ToDo",
+  img:"../image/taskflow.png",
+  descp:"A sleek and responsive task management tool that lets users add, track, and delete tasks easily. Built with JavaScript, HTML, and CSS.",
+  weblink:"https://todo-project-ten-chi.vercel.app/",
+  codelink:"https://github.com/satyajit9178/todo-project",
+},
+{
+  name:"Countdown Timer",
+  img:"../image/countdown.png",
+  descp:"A customizable countdown app that takes a future date and time, then shows the remaining days, hours, minutes, and seconds with a live progress bar.",
+  weblink:"https://countdown-project-gold.vercel.app/",
+  codelink:"https://github.com/satyajit9178/countdown-project",
+},
+{
+  name:"Chat Bot",
+  img:"../image/chatbot.png",
+  descp:"A responsive web-based chatbot that accepts user input and gives dynamic replies using JavaScript and DOM manipulation, mimicking a real-time conversation interface.",
+  weblink:"https://ai-chatbot-wheat-zeta.vercel.app/",
+  codelink:"https://github.com/satyajit9178/Ai-Chatbot",
+},
+{
+  name:"StopWatch",
+  img:"../image/stopwatch.png",
+  descp:"A simple stopwatch built with HTML, CSS, and JavaScript. It includes start, pause, and reset features with accurate time tracking.",
+  weblink:"https://sc-wd-2.vercel.app/",
+  codelink:"https://github.com/satyajit9178/SCT_WD_2",
+},
+{
+  name:"❌⭕ Tic Tac Toe Game",
+  img:"../image/tictactoe.png",
+  descp:"A classic Tic Tac Toe game built using HTML, CSS, and JavaScript. Playable on both desktop and mobile devices.",
+  weblink:"https://sct-wd-4-henna.vercel.app/",
+  codelink:"https://github.com/satyajit9178/SCT_WD_4",
+  }
+]
